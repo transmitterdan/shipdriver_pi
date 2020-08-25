6b07f33ce0a8342dbb928f00acae417887caea24
Rasbats <miker@members.swis.net>
2020-08-25 13:01:14 +0100

    Create version.h

---
+++ src/version.h
@@ -0,0 +1,5 @@
+#define PLUGIN_VERSION_MAJOR 2
+#define PLUGIN_VERSION_MINOR 4
+#define PLUGIN_VERSION_DATE "2020-05-09"
+#define PLUGIN_VERSION "ShipDriver_pi v2.4.-ov50"
+#define PLUGIN_VERSION_WITH_DATE "ShipDriver_pi v2.4.-ov50 released on 2020-05-09"
